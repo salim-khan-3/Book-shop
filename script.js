a03061568df1fa765d45f6f77a395e3d52fd1cc2
Md salim islam <salimislam0036@gmail.com>
2025-07-31 22:48:02 +0600

    navbar responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById("menuIcon");
+const navLinks = document.getElementById("navLinks");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
